6391f325e22aaea6dd32e266e9cf1ba26c4b419c
Alejandro <alexrioja98@gmail.com>
2021-10-10 18:37:05 +0200

    updates to code

--- main.cpp
+++ main.cpp
@@ -1,22 +1,25 @@
 #include "mbed.h"
+#include <cstdint>
 
 enum blindsState { raised, down, rising, lowering };
-InterruptIn user_btn(PB_2);
-AnalogIn ldr(A0);
-bool isSummer, operationLock = false;
-bool infoLock, seasonInfoLock, readLdr = true;
-Ticker ldr_5s, inform_2s;
 uint16_t ldrThreshold = 2000L;
 uint16_t ldrValue = 0;
-blindsState state = down;
+bool isSummer, operationLock = false;
+bool infoLock, seasonInfoLock, readLdr = true;
+float timeToOperate = 30.0f;
 blindsState new_state;
+Ticker ldr_5s, inform_2s;
 Timeout toBlinds;
+blindsState state;
+
 DigitalOut led_up(PB_5), led_middle(PB_6), led_down(PB_7), led_summer(PA_5);
+InterruptIn user_btn(PB_2);
+AnalogIn ldr(A0);
 
 void btn_handler(void) { seasonInfoLock = true; }
+void read_ldr_handler() { readLdr = true; }
+void inform_state_handler() { infoLock = true; }
 
-void read_ldr() { readLdr = true; }
-void inform_state() { infoLock = true; }
 void inform(blindsState new_state) {
   printf("Season---> %s", isSummer ? "Summer" : "Winter");
   printf("________Sensor readings--->%d", ldrValue);
@@ -52,13 +55,12 @@ void operationFinished() {
   else
     state = raised;
 }
-
 blindsState transitionState(bool up) {
   operationLock = true;
   switch (state) {
   case down:
     if (up) {
-      toBlinds.attach(operationFinished, 10.0f);
+      toBlinds.attach(operationFinished, timeToOperate);
       return rising;
     } else
       operationLock = false;
@@ -69,7 +71,7 @@ blindsState transitionState(bool up) {
       operationLock = false;
       return raised;
     } else {
-      toBlinds.attach(operationFinished, 10.0f);
+      toBlinds.attach(operationFinished, timeToOperate);
       return lowering;
     }
     break;
@@ -80,10 +82,11 @@ blindsState transitionState(bool up) {
 }
 
 int main() {
+  state=down;
   user_btn.mode(PullDown);
   user_btn.rise(btn_handler);
-  ldr_5s.attach(read_ldr, 5.0f);
-  inform_2s.attach(inform_state, 2.0f);
+  ldr_5s.attach(read_ldr_handler, 5.0f);
+  inform_2s.attach(inform_state_handler, 2.0f);
   while (true) {
     if (readLdr) {
       readLdr = false;
@@ -122,140 +125,3 @@ int main() {
     sleep();
   }
 }
-/*
-int main(){
-        ldr_2s.attach(read_ldr, 2.0f);
-        while (1) {
-        if (readLdr) {
-      readLdr = false;
-      ldrValue = ldr.read_u16();
-                        printf("%d\n", ldrValue);
-    }
-        }
-}
-*/
-
-/*
-#include "mbed.h"
-
-enum blindsState { raised, down, rising, lowering };
-InterruptIn user_btn(PB_2);
-AnalogIn ldr(A0);
-bool isSummer, readLdr, isStateChanged, operationLock = false;
-bool infoLock, seasonInfoLock = true;
-Ticker ldr_2s;
-uint16_t ldrThreshold = 2000L;
-uint16_t ldrValue = 0;
-blindsState state = down;
-blindsState new_state;
-Timeout toBlinds;
-
-void btn_handler(void) {
-  isSummer = !isSummer;
-  seasonInfoLock = true;
-}
-
-void read_ldr() { readLdr = true; }
-
-void inform_state(blindsState new_state) {
-  printf("Current State-->");
-  switch (new_state) {
-  case down:
-    printf("down\n");
-    break;
-  case lowering:
-    printf("lowering\n");
-    break;
-  case rising:
-    printf("rising\n");
-    break;
-  case raised:
-    printf("raised\n");
-    break;
-  }
-}
-void operationFinished() {
-  operationLock = false;
-  infoLock = true;
-  if (state == lowering)
-    state = down;
-  else
-    state = raised;
-}
-
-blindsState transitionState(bool up) {
-  operationLock = true;
-  switch (state) {
-  case down:
-    if (up) {
-      toBlinds.attach(operationFinished, 10.0f);
-      return rising;
-    } else
-      operationLock = false;
-    return down;
-    break;
-  case raised:
-    if (up) {
-      operationLock = false;
-      return raised;
-    } else {
-      toBlinds.attach(operationFinished, 10.0f);
-      return lowering;
-    }
-    break;
-  default:
-    operationLock = false;
-    return state;
-  }
-}
-
-int main() {
-  user_btn.mode(PullDown);
-  user_btn.rise(btn_handler);
-  ldr_2s.attach(read_ldr, 2.0f);
-  while (true) {
-    if (readLdr) {
-      readLdr = false;
-      ldrValue = ldr.read_u16();
-      if (isStateChanged) {
-        // operationLock=false;
-        isStateChanged = false;
-      }
-      printf("%d\n", ldrValue);
-    }
-    if (seasonInfoLock) {
-      seasonInfoLock = false;
-      printf("\n-------------------------------------------------------\n");
-      printf("We are in---->%s", isSummer ? "Summer" : "Winter");
-      printf("\n-------------------------------------------------------\n\n");
-    }
-    if (infoLock) {
-      infoLock = false;
-      inform_state(state);
-    }
-    if (!operationLock) {
-      if (isSummer) {
-        if (ldrValue > ldrThreshold) {
-          // printf("Detected high light levels\n");
-          new_state = transitionState(false);
-        } else {
-          // printf("Detected low light levels\n");
-          new_state = transitionState(true);
-        }
-      } else {
-        if (ldrValue > ldrThreshold) {
-          // printf("Detected high light levels\n");
-          new_state = transitionState(true);
-        } else {
-          // printf("Detected low light levels\n");
-          new_state = transitionState(false);
-        }
-      }
-      if (new_state != state)
-        infoLock = isStateChanged = true;
-      state = new_state;
-    }
-    sleep();
-  }
-}
-*/
